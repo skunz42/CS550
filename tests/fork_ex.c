751a4cb424807226fb36c263d8f479b4ddc5b166
Sean Kunz <skunz598@gmail.com>
2021-02-22 18:06:02 -0500

    class notes

---
+++ tests/fork_ex.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main()
+{
+	pid_t pid;
+	int status;
+	int ret;
+
+	pid = fork();
+ 
+	if (pid < 0) { 
+		perror("fork failed:"); 
+		exit(1); 
+	} 
+
+	if (pid == 0) { // Child executes this block
+		printf("This is the child\n"); 
+		exit(0); 
+	} 
+
+	if (pid > 0) { //Parent executes this block
+		printf("This is parent. The child is %d\n", pid); 
+
+		ret = waitpid(pid, &status, 0);
+		if (ret < 0) {
+			perror("waitpid failed:");
+			exit(2); 
+		}
+
+
+		printf("Child exited with status %d\n", WEXITSTATUS(status));
+	} 
+
+	return 0;
+}
+
